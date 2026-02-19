24f9d44d49e59f3ca27cda4b6b76a83101a4152f
rohit <rohitkodali92@gmail.com>
2026-02-19 21:06:00 +0530

    hp dell

--- 03-condition.sh
+++ 03-condition.sh
@@ -7,7 +7,7 @@ if [ $NUMBER -lt 10 ]; then
 elif [ $NUMBER -ne 15 ]; then
     echo "given number $NUMBER is not equal to 15"
 else
-    echo "given number $NUMBER is greater then or equal to 10 15"
+    echo "given number $NUMBER is greater then or equal to 15"
 fi
 
 #greater than= -gt
