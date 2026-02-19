56ddeb38271f955c5fa2321cbee5b5305cd29717
rohit <rohitkodali92@gmail.com>
2026-02-19 21:39:07 +0530

    condition

--- 04-condition.sh
+++ 04-condition.sh
@@ -3,7 +3,7 @@
 echo "plese enter the number"
 read NUMBER
 
-if [ $((NUMBER % 5)) -eq 0 ]; then
+if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "given number $NUMBER is EVEN"
 else
     echo "given number $NUMBER is ODD"
