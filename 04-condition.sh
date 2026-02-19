6152417fbf12f1ba36c7e2719cf5981568def3ee
rohit <rohitkodali92@gmail.com>
2026-02-19 21:30:57 +0530

    hp dell

---
+++ 04-condition.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "plese enter the number"
+read number
+
+if [ $(($NUMBER % 5))-ne 0 ]; then
+    echo "given number $NUMBER is EVEN"
+else
+    echo "given number $NUMBER is ODD"
+fi
